5fbed5e2960b53579bb90c0dace339924966ecdc
Rusten Line <rusten.line@ga.gov.au>
2022-06-20 19:53:08 +0400

    added CRUD methods

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPDotNetCoreWebAPI.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set;}
+
+    }
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ASPDotNetCoreWebAPI.Models
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
